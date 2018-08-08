3726fb83fb6c8cc08a69b6dfd9b5c33f13f8d48b
Freeman3389 <dragoon_lee@hotmail.com>
2018-08-08 21:50:42 +0800

    Add Line Notify and Automations

--- externalscripts/line_notify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#!/bin/bash
-foo=$(cat)
-bar=${foo// /+}
-/usr/bin/curl "http://ipadd:2323/?cmd=textToSpeech&text=$bar&password=pass" >> /dev/null
-/usr/bin/curl -X POST -H "Authorization: Bearer ${TOKEN}" \
- -F "message=${message}" https://notify-api.line.me/api/notify >> /dev/null
-
